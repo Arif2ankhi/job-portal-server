1b4006798b651674393188fb9491634690c9e590
Arif2ankhi <arif2ankhi@gmail.com>
2024-12-18 01:54:24 +0000

    Axios functionality added

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ const verifyToken = (req, res, next) => {
     
 }
 
-// const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nsvzy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const uri = "mongodb+srv://test:owoWCuYsgj0SVJI1@cluster0.nsvzy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nsvzy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
